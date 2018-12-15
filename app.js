78c1bcf89a6593b2eeaea6d356c6c4c255958867
CuteLyp <CuteLyp@users.noreply.github.com>
2018-12-15 20:37:50 +0000

    Update app.js

--- app.js
+++ app.js
@@ -12,38 +12,78 @@ firebase.initializeApp(config);
 (function() {
     const dbRef = firebase.database().ref('All/Location');
     dbRef.on('child_added', snap => {
+        const buildingList = document.getElementById('buildingList');
         const div = document.createElement('div');
-        div.innerText = snap.key + ":";
+        const b = document.createElement('b');
+        b.innerHTML = snap.key + ":";
+        div.appendChild(b);
+        div.setAttribute("class", "building");
         div.id = snap.key;
         buildingList.appendChild(div);
-        const sideBar = document.getElementById('side-bar');
-        const sides = document.createElement('div');
-        const checkboxForSide = document.createElement('input');
-        checkboxForSide.onclick = hideFunction;
+        
+        const sideBar = document.getElementById('homeSubmenu');
+        const sides = document.createElement('li');
+        const sides_a = document.createElement('a');
+        sides_a.onclick = hideFunction;
         sides.id = snap.key + '_sides';
-        checkboxForSide.setAttribute("type", "checkbox");
-        checkboxForSide.setAttribute("name", snap.key);
-        sides.innerText = snap.key;
+        sides.setAttribute("name", snap.key);
+        sides_a.innerHTML = snap.key;
+        sides_a.style.cursor = "pointer";
+        sides_a.setAttribute("name", snap.key);
+        sides.appendChild(sides_a);
         sideBar.appendChild(sides);
-        sides.appendChild(checkboxForSide);
 
-        snap.forEach(function(childSnap) {
+        let index = 0;
+        let queue = [];
+        snap.forEach(childSnap => {
             var roomkey = childSnap.key;
-            const ul = document.createElement('ul');
+            const ul = document.createElement('div');
             div.appendChild(ul);
-            ul.innerText = roomkey + ":";
+            ul.innerHTML = snap.key + " " + roomkey + ":";
             ul.setAttribute("building", snap.key);
+            ul.setAttribute("class", "col-sm");
             ul.id = roomkey;
             const selected = document.createElement('input');
             selected.setAttribute("type", "checkbox");
             selected.setAttribute("id", roomkey);
             selected.setAttribute("name", snap.key);
             ul.appendChild(selected);
+            //div.appendChild(ul);
+            if(index % 2 != 0) {
+                const row = document.createElement('div');
+                row.setAttribute("class", 'row');
+                queue.push(ul);
+                div.appendChild(row);
+                while(queue.length != 0) {
+                    row.appendChild(queue.shift());
+                }
+            } else {
+                queue.push(ul);
+            }
+            
+            index++;
 
-            childSnap.forEach(function(attributes) {
-                const li = document.createElement('li');
-                ul.appendChild(li);
-                li.innerText = attributes.key + ": " + attributes.val();
+            childSnap.forEach(attributes => {
+                if(attributes.key == 'aimage') {
+                    const img = document.createElement('img');
+                    img.setAttribute("src", attributes.val());
+                    img.style.height = '100px';
+                    img.style.width = '100px';
+                    ul.appendChild(img);
+                } else if(attributes.key == 'price' || attributes.key == 'availability') {
+                    const span = document.createElement('span');
+                    const li = document.createElement('li');
+                    li.innerHTML = attributes.key + ': ';
+                    li.appendChild(span);
+                    li.setAttribute("name", attributes.key);
+                    ul.appendChild(li);
+                    span.innerHTML = attributes.val();
+                } else {
+                    const li = document.createElement('li');
+                    ul.appendChild(li);
+                    li.setAttribute("name", attributes.key);
+                    li.innerHTML = attributes.key + ': ' + attributes.val();
+                }
             });
         });
     });
@@ -51,27 +91,63 @@ firebase.initializeApp(config);
     //change included(replace and change)
     dbRef.on('child_changed', snap => {
         const building = document.getElementById(snap.key);
-        building.innerText = "";
-        building.innerText = snap.key;
+        building.innerHTML = "";
+        const b = document.createElement('b');
+        b.innerHTML = snap.key + ":";
+        building.appendChild(b);
 
-        snap.forEach(function(childSnap) {
+        let index = 0;
+        let queue = [];
+        snap.forEach(childSnap => {
             var roomkey = childSnap.key;
             const ul = document.createElement('ul');
             building.appendChild(ul);
-            ul.innerText = roomkey + ":";
+            ul.innerHTML = snap.key + " " + roomkey + ":";
             ul.setAttribute("building", snap.key);
+            ul.setAttribute("class", "col-sm");
             ul.id = roomkey;
             const selected = document.createElement('input');
             selected.setAttribute("type", "checkbox");
             selected.setAttribute("id", roomkey);
-            selected.setAttribute("name", "selectedRoom");
+            selected.setAttribute("name", snap.key);
             ul.appendChild(selected);
+            
+            if(index % 2 != 0) {
+                const row = document.createElement('div');
+                row.setAttribute("class", 'row');
+                queue.push(ul);
+                building.appendChild(row);
+                while(queue.length != 0) {
+                	row.appendChild(queue.shift());
+                }
+            } else {
+            	queue.push(ul);
+            	
+            }
+            index++;
 
-            childSnap.forEach(function(attributes) {
-                const li = document.createElement('li');
-                ul.appendChild(li);
-                li.innerText = attributes.key + ": " + attributes.val();
-            });           
+            childSnap.forEach(attributes => {
+                if(attributes.key == 'aimage') {
+                    const img = document.createElement('img');
+                    img.setAttribute("src", attributes.val());
+                    img.style.height = '100px';
+                    img.style.width = '100px';
+                    ul.appendChild(img);
+                } else if(attributes.key == 'price' || attributes.key == 'availability') {
+                    const span = document.createElement('span');
+                    const li = document.createElement('li');
+                    li.innerHTML = attributes.key + ': ';
+                    li.appendChild(span);
+                    li.setAttribute("name", attributes.key);
+                    ul.appendChild(li);
+                    span.innerHTML = attributes.val();
+                } else {
+                    const li = document.createElement('li');
+                    ul.appendChild(li);
+                    li.setAttribute("name", attributes.key);
+                    li.innerHTML = attributes.key + ': ' + attributes.val();
+                }
+            });        
         });
     });
 
@@ -119,7 +195,6 @@ firebase.initializeApp(config);
     
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if(!firebaseUser) {
-            console.log('not logged in');
             btnLogout.style.display = "none";
         } else {
             var alogin = document.getElementById("aLogin");
@@ -128,6 +203,7 @@ firebase.initializeApp(config);
             //console.log(firebaseUser);
         }
     });
+    
 }());
 
 /*
@@ -140,8 +216,8 @@ console.log(ref.path);
 const rooms = document.getElementsByTagName("input");
 function checkCheckboxs () {
     //check room only one can be choose
-    var count = 0;
-    var selectedNumArray = new Array();
+    let count = 0;
+    let selectedNumArray = [];
     for(let i = 0; i < rooms.length; i++) {
         if(rooms[i].checked) {
             selectedNumArray[count] = i;
@@ -155,20 +231,21 @@ function checkCheckboxs () {
         alert("You have not choose any room yet!");
     } else {
         //console.log(selectedNum.length);
-        var selectedNum = selectedNumArray[0];
-        bookRoom(selectedNum, count); 
+        const selectedNum = selectedNumArray[0];
+        bookRoom(selectedNum, count);
     }
 }
 
 function bookRoom (selectedNum) {
     //inspect the parameter which one is choose
     if(rooms[selectedNum].checked && confirm("Are you sure choose this room?")) {
-        var buildingName = rooms[selectedNum].name;
-        var roomKey = rooms[selectedNum].id;
+        const buildingName = rooms[selectedNum].name;
+        const roomKey = rooms[selectedNum].id;
         firebase.auth().onAuthStateChanged(firebaseUser => {
             if(firebaseUser) {
                 //email cannot as the key in firebase
-                var email = firebaseUser.email;
+                console.log(firebaseUser.uid);
+                const email = firebaseUser.email;
                 queryStudentId(email, buildingName, roomKey);
             } else {
                 //
@@ -181,11 +258,11 @@ function bookRoom (selectedNum) {
 //var email = '123@test.ie';
 //console.log(queryStudentId(email));
 function queryStudentId(email, buildingName, roomKey) {
-    var testRef = firebase.database().ref("/All/StudentList");
+    const testRef = firebase.database().ref("/All/StudentList");
     testRef.orderByChild("email").equalTo(email).on("value", snap => {
         snap.forEach(childSnap => {
-            var studentId = childSnap.child("studentId").val();
-            var name = childSnap.child("name").val();
+            const studentId = childSnap.child("studentId").val();
+            const name = childSnap.child("name").val();
             if(studentId != null && studentId != "") {
                 //console.log(studentId);
                 confirmRepeatSelect(studentId, name, buildingName, roomKey);
@@ -193,11 +270,17 @@ function queryStudentId(email, buildingName, roomKey) {
                 console.log("Not found");
             }
         });
+    }, error => {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log("successful");
+        }
     });    
 }
 
 function confirmRepeatSelect (studentId, name, buildingName, roomKey) {
-    var ref = firebase.database().ref('All/BookedList');
+    const ref = firebase.database().ref('All/BookedList');
     ref.once("value").then(snap => {
         if(!snap.child(studentId).exists()) {
             checkAvailability(studentId, name, buildingName, roomKey);
@@ -210,11 +293,10 @@ function confirmRepeatSelect (studentId, name, buildingName, roomKey) {
 function checkAvailability(studentId, name, buildingName, roomKey) {
     const ref = firebase.database().ref('All/Location'+'/'+buildingName+'/'+roomKey);
     ref.once('value').then(snap => {
-        var availability = snap.val().availability;
+        const availability = snap.val().availability;
         //var price = snap.val().price;
         if(availability > 0) {
             updateAvilability(buildingName, roomKey, availability);
-            console.log(1);
             recordTheRoom(studentId, name, buildingName, roomKey);
         } else if(availability < 1) {
             alert("This room was full!");
@@ -223,14 +305,13 @@ function checkAvailability(studentId, name, buildingName, roomKey) {
 }
 
 function updateAvilability(buildingName, roomKey, availability) {
-    var after = availability - 1;
-    var updateData = {
+    const after = availability - 1;
+    let updateData = {
         availability: after
     };
-    var path = {};
+    let path = {};
     path['All/Location'+'/'+buildingName+'/'+roomKey+'/availability'] = after;
     firebase.database().ref().update(path);
-    //callback function here
     window.location = 'order.html';
 }
 
@@ -244,8 +325,8 @@ function recordTheRoom(studentId, name, buildingName, roomKey) {
 }
 
 function hideFunction() {
-    var name = this.name;
-    var buildingName = document.getElementById(name);
+    let name = this.name;
+    const buildingName = document.getElementById(name);
     if(buildingName.style.display != "none") {
         buildingName.style.display = "none";
     } else {
@@ -253,45 +334,21 @@ function hideFunction() {
     }
 }
 
-function relist(number) {
-    var roomArray = [];
-    var ref = firebase.database().ref('All/Location').orderByChild('price');
-    ref.once("value").then(function (snap) {
-        snap.forEach(function (child) {
-            child.forEach(function (childsnap) {
-                roomArray.push(childsnap);
-            })
-        })
-        var result = mergeSort(roomArray, number);
-        //
-        if(number == 1) {
-            //遍历
-            for(let i = 0; i < result.length; i++) {
-                console.log(result[i].val().price);
-            }
-            //return result;
-        } else {
-            for(let i = 0; i < result.length; i++) {
-                console.log(result[i].val().availability);
-            }
-        }
-    });
-}
 
 //MergeSort
 function mergeSort(array, number) {
     if(array.length < 2) {
         return array;
     }
-    var mid = parseInt(array.length / 2);
-    var left = array.slice(0, mid);
-    var right = array.slice(mid);
+    let mid = parseInt(array.length / 2);
+    let left = array.slice(0, mid);
+    let right = array.slice(mid);
     return merge(mergeSort(left, number), mergeSort(right, number), number);
 }
 
 function merge(left, right, number) {
-    var result = [];
-    var low = 0, high = 0;
+    let result = [];
+    let low = 0, high = 0;
     while(low < left.length && high < right.length) {
         if(number == 1) {
             if(left[low].val().price > right[high].val().price) {
@@ -317,13 +374,51 @@ function merge(left, right, number) {
 }
 
 function listByPrice() {
-    var newList = relist(1);
-    console.log(newList);
+    const list = document.getElementsByClassName('col-sm');
+    let newList = document.getElementById('buildingList');
+    const result = [...list];
+    result.sort((a,b) => {
+        return a.getElementsByTagName('span')[1].innerHTML - b.getElementsByTagName('span')[1].innerHTML
+    })
+    newList.innerHTML = "";
+    let queue = [];
+    for(let i = 0; i < result.length; i++) {
+        if(i % 2 != 0) {
+            const row = document.createElement('div');
+            row.setAttribute("class", 'row');
+            queue.push(result[i]);
+            newList.appendChild(row);
+            while(queue.length != 0) {
+                row.appendChild(queue.shift());
+            }
+        } else {
+            queue.push(result[i]);        
+        }
+    }
 }
 
 function listByAvailability() {
-    var newList = relist(2);
-    console.log(newList);
+    const list = document.getElementsByClassName('col-sm');
+    let newList = document.getElementById('buildingList');
+    const result = [...list];
+    result.sort((a,b) => {
+        return b.getElementsByTagName('span')[0].innerHTML - a.getElementsByTagName('span')[0].innerHTML
+    })
+    newList.innerHTML = "";
+    let queue = [];
+    for(let i = 0; i < result.length; i++) {
+        if(i % 2 != 0) {
+            const row = document.createElement('div');
+            row.setAttribute("class", 'row');
+            queue.push(result[i]);
+            newList.appendChild(row);
+            while(queue.length != 0) {
+                row.appendChild(queue.shift());
+            }
+        } else {
+            queue.push(result[i]);        
+        }
+    }
 }
 
 
@@ -348,6 +443,7 @@ function countTime() {
             timer.innerHTML = "Please check your payment, if you have any problem pleas contact staff!";
             note.innerHTML = "Note: If you have not finished your payment, your appointment could be canceled.";
             clearInterval(set);
+            //callback function
         } else if(second == 0) {
             second = 59;
             minute--;
@@ -356,98 +452,3 @@ function countTime() {
         }
     }, 1000);
 }
-
-//Order page
-
-//Management page
-//(Room Building and Student)
-
-
-/*
-function test(x, y) {
-    return x + y;
-}
-function test2() {
-    var i = test(3, 5);
-    return i;
-}
-function test3() {
-    return test2();
-}
-
-var z = test3();
-console.log(z);
-
-//第一个方法执行完毕之后再调用第二个方法保证同步方法;
-//注册的时候添加学号 /email/studentid
-
-                    callback function
-                    function (error) {
-                        console.log(1);
-                        if(error) {
-                            console.error(error);
-                        } else {
-                            //window.location = 'order.html';
-                            console.log("success!");
-                        }
-                    }
-                    
-
-    var ref = firebase.database().ref("All/BookedList");
-    ref.orderByChild("studentId").equalTo(18251993).on("value", function(snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            console.log(childSnapshot.child("studentId").val());
-        })
-    })
-
-
-
-
-checkRecord('user1');
-function checkRecord (email) {
-    var ref = firebase.database().ref('All/BookedList');
-    ref.once("value").then(function (snap) {
-        if(!snap.child(email).exists()) {
-            function recordTheRoom (email, buildingname, roomkey, semester) {
-                firebase.database().ref('All/BookedList/' + email).set({
-                    buildingName: buildingname,
-                    roomKey: roomKey,
-                    semester: semester
-                });
-            }
-        } else {
-            function error() {
-               alert("You have booked room!");
-            }
-        }
-    });
-}
-
-    jsonObject
-    
-    
-    setTimeout(function () {
-        console.log(checkRecord('user1'));
-    }, 5);
-
-
-    //dbRefList.on('child_added', snap => console.log(snap.val()));  有效
-    
-    dbRefCakeList.on('child_added', snap => {
-        const ul = document.createElement('ul');
-        ul.innerText = snap.key + ":" + snap.val();
-        ul.id = snap.key;
-        cakeRooms.appendChild(ul);
-    });
-    
-    dbRefList.on('child_changed', snap => {
-        const liChanged = document.getElementById(snap.key);
-        liChanged.innerText = snap.val();
-    });
-
-    dbRefList.on('child_removed', snap => {
-        const liToRemove = document.getElementById(snap.key);
-        liToRemove.remove();
-    });
-
-    */
